6ed946d1b69ed415c02fd4c9c203838afdfb34eb
flaviatovo <flaviatovo@gmail.com>
2012-10-07 01:35:53 +0200

    Organizing code into folders, shell script removed

--- afl.sh
+++
@@ -1,6 +0,0 @@
-g++ -Wall -c Matrix.cpp
-g++ -Wall -c Vector.cpp
-g++ -Wall -c aflComponent.cpp
-g++ -Wall -c aflAplicativo.cpp
-g++ -Wall -c afl.cpp
-g++ -Wall -o afl Matrix.o Vector.o aflComponent.o aflAplicativo.o afl.o
